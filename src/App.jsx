175e686c572351ffa5f372923ced3bf683c4b43c
NatSoull <minthukhant326@gmail.com>
2023-08-27 14:40:48 +0630

    just-break-time

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+import Navbar from "./components/Navbar";
+// import ('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500&family=Montserrat:wght@300;400;500;600;700&family=Tektur&display=swap');
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+};
+
+export default App;
